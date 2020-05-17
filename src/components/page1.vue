82953abe7023aebc148b8849141e2f64a0658c6f
toru-x <qubilaihan@gmail.com>
2020-05-17 14:59:22 +0900

    axios動作確認
    postでデータ取得動作

--- src/components/page1.vue
+++ src/components/page1.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>
-    <p>
-      ここはページ１です。
-    </p>
-    <router-link to="/page2">Go to page2</router-link>
+  <div id="search-ip">
+    <div>
+      <div>
+        <input @click="getIp" type="button" value="IPを取得" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+export default {
+  name: "SearchIp",
+  methods: {
+    getIp() {
+      this.axios
+//        .post("http://www.tic-keisokuki.com:5000/pure_flow_data_send")
+//        .post("http://www.tic-keisokuki.com:5000/thermograph_data_send")
+        .post("http://www.tic-keisokuki.com:5000/thp_data_send")
+        .then(response => {
+          alert(JSON.stringify(response.data));
+          console.log(response.data)
+})
+        .catch(e => {
+          alert(e);
+        });
+    }
+  }
+};
 </script>
-
-<style>
-</style>
