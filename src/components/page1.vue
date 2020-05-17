6d7c775a894e86e8f054ba8a0df4bd86e81c3c1f
toru-x <qubilaihan@gmail.com>
2020-05-17 15:26:50 +0900

    axios
    データ送信ボタン追加

--- src/components/page1.vue
+++ src/components/page1.vue
@@ -1,18 +1,28 @@
 <template>
-  <div id="search-ip">
+<div id="axios_test">
+  <div id="axios-ip">
     <div>
       <div>
-        <input @click="getIp" type="button" value="IPを取得" />
+        <input @click="receiveIp" type="button" value="測定値を取得" />
       </div>
     </div>
   </div>
+
+  <div id="send-ip">
+    <div>
+      <div>
+        <input @click="sendIp" type="button" value="測定値を送信" />
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: "SearchIp",
+  name: "ReceiveIp",
   methods: {
-    getIp() {
+    receiveIp() {
       this.axios
 //        .post("http://www.tic-keisokuki.com:5000/pure_flow_data_send")
 //        .post("http://www.tic-keisokuki.com:5000/thermograph_data_send")
@@ -24,7 +34,20 @@ export default {
         .catch(e => {
           alert(e);
         });
+    },
+    sendIp() {
+      this.axios
+        .post("http://www.tic-keisokuki.com:5000/thp_data_send")
+        .then(response => {
+          alert(JSON.stringify(response.data));
+          console.log(response.data)
+        })
+        .catch(e => {
+          alert(e);
+        });
     }
   }
 };
+
+
 </script>
